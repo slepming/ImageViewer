2c91b3ae35529c282b5b32dc9b845c6069387178
slepming <siskaura06@gmail.com>
2025-11-10 20:02:39 +0300

    moved shaders to file shaders

---
+++ shaders/rect.vert
@@ -0,0 +1,10 @@
+
+#version 450
+
+layout(location = 0) in vec2 position;
+layout(location = 0) out vec2 tex_coords;
+
+void main() {
+  gl_Position = vec4(position, 0.0, 1.0);
+  tex_coords = position / 1.8 + 0.5;
+}
--- src/app.rs
+++ src/app.rs
@@ -618,36 +618,12 @@ fn window_size_dependent_setup(
 
 mod vs {
     vulkano_shaders::shader! {
-        ty: "vertex",
-        src: r"
-        #version 450
-
-        layout(location = 0) in vec2 position;
-        layout(location = 0) out vec2 tex_coords;
-
-        void main() {
-            gl_Position = vec4(position, 0.0, 1.0);
-            tex_coords = position / 1.8 + 0.5;
-        }
-
-        ",
+        bytes: "shaders/rect.vert.spv"
     }
 }
 
 mod fs {
     vulkano_shaders::shader! {
-        ty: "fragment",
-        src: r"
-        #version 450
-
-        layout(location = 0) in vec2 tex_coords;
-        layout(location = 0) out vec4 f_color;
-
-        layout(set = 0, binding = 0) uniform sampler s;
-        layout(set = 0, binding = 1) uniform texture2D tex;
-
-        void main() {
-            f_color = texture(sampler2D(tex, s), tex_coords);
-        }"
+        bytes: "shaders/rect.frag.spv"
     }
 }
