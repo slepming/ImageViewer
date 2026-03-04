54c4d53f81bd0e901e5d10a8280ded2554523aee
slepming <siskaura06@gmail.com>
2026-03-04 22:01:57 +0300

    first argument moved to another variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod window;
-
 use std::{env, process::exit};
 
 use anyhow::Ok;
@@ -12,14 +11,17 @@ mod shaders;
 fn main() -> Result<(), anyhow::Error> {
     pretty_env_logger::init_timed();
     let args: Vec<String> = env::args().collect();
+
     if args.len() < 2 {
         warn!("app haven't argument path to image. Please, set up argument path");
         exit(-1);
     }
+
     info!("{}", args[1]);
+    let image_path = &args[1];
     let event_loop = EventLoop::new()?;
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
-    let mut app = App::new(&args[1], &event_loop);
+    let mut app = App::new(image_path, &event_loop);
     event_loop.run_app(&mut app)?;
     Ok(())
 }
